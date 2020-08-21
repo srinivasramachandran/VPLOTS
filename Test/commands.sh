fcae6b630a058007bce88ada020f7f4865e81b83
Srinivas Ramachandran <srinivas.ramachandran@ucdenver.edu>
2020-08-21 11:05:52 -0600

    COMB PDF

--- Test/commands.sh
+++ Test/commands.sh
@@ -1,4 +1,4 @@
 #perl ../code/vplot_bed.pl ../lib/Reb1.list ../Dat/GSM1647331_Reb1_ChEC_seq_30s_1.bed.gz > Reb1_ChEC_single.vplot
 #perl ../code/vplot_bed_list.pl ../lib/Reb1.list FILE_LIST > Reb1_ChEC_COMB.vplot
 #sh ../scripts/vplot_figure.sh Reb1_ChEC_single.vplot
-sh ../scripts/vplot_figure.sh Reb1_ChEC_single.vplot Reb1_ChEC_COMB.vplot
+sh ../scripts/vplot_figure.sh Reb1_ChEC_COMB.vplot
